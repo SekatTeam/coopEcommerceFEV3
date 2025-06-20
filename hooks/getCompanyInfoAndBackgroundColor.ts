38a0cd4f5a005be09b3a5458cffa1a4c66417324
TeeAkinlade <taiye.akinlade1@gmail.com>
2025-06-20 12:38:44 +0100

    Changes

--- hooks/getCompanyInfoAndBackgroundColor.ts
+++ hooks/getCompanyInfoAndBackgroundColor.ts
@@ -1,6 +1,8 @@
 export async function getCompanyInfoAndBackgroundColor() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL2;
+  const version = process.env.VERSION;
   const res = await fetch(
-    "https://coopwebsitev3-cpdpgqcqaqb6ftcv.centralus-01.azurewebsites.net/app/v1/LandingPage/company-info-and-background-color",
+    `${baseUrl}/app/${version}/LandingPage/company-info-and-background-color`,
     {
       headers: {
         requestIdentity: "demo.com",
